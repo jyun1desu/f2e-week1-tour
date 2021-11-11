28db1accb6428ab1bb440467ee2468eb4bd8a8a7
jyun1desu <jyun1desu@25sprout.com>
2021-11-11 17:32:08 +0800

    feat: add fake AttractionCard

---
+++ src/components/atoms/AttractionCard/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as LocationIcon } from 'images/icon/location.svg'
+
+import style from "./index.module.scss";
+
+const AttractionCard = ({ }) => {
+    return (
+        <Link
+            className={style.attractionCard}
+            to="/"
+        >
+            <div className={style.content}>
+                <div className={style.cover}>
+                    <img src="https://cdn04.pinkoi.com/pinkoi.site/event/miffy-2020/03_Theme%20shop_content.png" alt="cover" />
+                </div>
+                <div className={style.info}>
+                    <h3 className={style.name}>景美溪河濱自行車道</h3>
+                    <p className={style.address}>
+                        <LocationIcon />
+                        <span>台北市 大安區</span>
+                    </p>
+                    <ul className={style.tags}>
+                        <li>自然風景</li>
+                        <li>自然風景</li>
+                        <li>自然風景</li>
+                    </ul>
+                </div>
+
+            </div>
+        </Link>
+    );
+};
+
+export default AttractionCard;
